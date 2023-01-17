b65d742ee12a6592f5e8f30f216adde89f35503d
akash9753 <64675019+akash9753@users.noreply.github.com>
2023-01-17 17:19:08 +0530

    Add files via upload
    1st

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css"
+import FilterMapMethod from "./pages/FilterMapMethod";
+import SpreadOpr from "./pages/SpreadOpr";
+import AntdForm from "./pages/AntdForm";
+import MultiSelect from "./pages/MultiSelect";
+import Flex from "./pages/Flex";
+import Checkbox from "./pages/checkbox/Checkbox";
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     {/* <FilterMapMethod/>  */}
+      {/*<SpreadOpr/>*/}
+      {/* <AntdForm/> */}
+      {/*<MultiSelect/>*/}
+      {/* <Flex/> */}
+      <Checkbox/>
+    </div>
+  );
+}
+
+export default App;
