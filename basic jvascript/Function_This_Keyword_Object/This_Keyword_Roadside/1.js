d0575f8d9b407d5ec0f942b72e7e63321158f0d3
akash9753 <64675019+akash9753@users.noreply.github.com>
2023-03-19 22:34:16 +0530

    new

--- basic jvascript/Function_This_Keyword_Object/This_Keyword_Roadside/1.js
+++ basic jvascript/Function_This_Keyword_Object/This_Keyword_Roadside/1.js
@@ -16,7 +16,7 @@ let user = {
     childObj:{
         newName:"akash patel",
         getDetails(){
-            console.log(this.newName,"and",this.name);
+            console.log(this.newName,"and",this.name);//
         }
     }
 }
@@ -46,10 +46,82 @@ let userAaa = {
     age:24,
     getDetails(){
         let newName="akash patel",
-        nestedArrow=()=> console.log(this,newName,"and",this.name);
+        nestedArrow=()=> console.log(this,newName,"and",this.name);//{ name: 'akash', age: 24, getDetails: [Function: getDetails] } akash patel and akash
         nestedArrow();
     }
 }
 //userAaa.getDetails();
 //-------------------------------------------
 //how this keyword work inside  class 
+class user56{
+    constructor(n){
+        this.name = n;
+    }
+    getName(){
+        console.log(this.name);
+    }
+}
+
+const User  = new user56("Piyush");
+//User.getName();
+//--------------------------------------------
+const user78 = {
+   firstName:"akash",
+   getName(){
+    const firstName = "akash patel";
+    return this.firstName;
+   }
+}
+// console.log(user78.getName());
+//---------------------------------------------
+function makeUser(){
+    return {
+        name:"John",
+        ref(){
+            return this;
+        }
+    }
+}
+
+let user15 = makeUser()
+// console.log(user15.ref().name);
+//-----------------------------------------------
+const user45 = {
+    name: "akash patel",
+    logMessage(){
+        console.log(this.name);
+    }
+}
+//setTimeout(function(){user45.logMessage()},1000)
+//------------------------------------------------
+var length = 4;
+
+function callback(){
+    console.log(this.length);
+}
+
+const object = {
+    length:5,
+    method(fn){
+        fn()
+    }
+}
+// object.method(callback)
+//----------------------------------------
+const calc = {
+    total:0,
+    add(a){
+        this.total += a;
+        return this;
+    },
+    multiply(a){
+        this.total *= a;
+        return this;
+    },
+    substract(a){
+        this.total -=a;
+        return this;
+    }
+};
+const result = calc.add(10).multiply(5).substract(30).add(10);
+console.log(result.total);
