3d873e398070a05395e46a23b0f0db785904b049
akash9753 <64675019+akash9753@users.noreply.github.com>
2023-03-22 17:35:26 +0530

    new

--- basic jvascript/call_bind_apply/call_bind_apply.js
+++ basic jvascript/call_bind_apply/call_bind_apply.js
@@ -48,3 +48,59 @@ var person56 = {age : 27}
 console.log(person24.getAge());
 console.log(person24.getAge.call(person56));
 //-------------------------------------------
+const animals = [
+    {spacies:"Lion", name:"King"},
+    {spacies:"Whale",name:"Queen"}
+]
+
+function printAnimals(i){
+    console.log(this);
+       this.print = function(){
+        console.log(this);
+        console.log("#"+ i + " " + this.spacies + ": " + this.name);
+    }
+    this.print();
+}
+for(let i = 0; i < animals.length; i++){
+    printAnimals.call(animals[i], i)
+}
+//------------------------------------------
+//append an array to another array
+const array = ["a","b"]
+
+const elements = [0,1,2];
+
+array.push.apply(array,elements);
+console.log(array);
+//-------------------------------------------
+const numbers = [5,6,2,3,7];
+
+console.log(Math.max.apply(null,numbers));
+//-------------------------------------------
+function f(){
+    console.log(this);
+}
+let user = {
+    g: f.bind(null)
+}
+user.g()
+//-------------------------------------------
+//arrow function with call bind and apply
+//Arrow function => parent normal function | wont affact from call bind apply
+const agee = 10;
+ var person96 = {
+    name:"akash",
+    age:20,
+    getArrow:()=>console.log(this.age),//nothing print
+    getArrow:()=>console.log(this),//window obj
+    getAge:function(){
+        console.log(this.age);
+    }
+ }
+
+ var person2 = {age:24}
+ person.getArrow.call(person2)
+ person.getAge.call(person2);
+ //--------------------------------------------
+ //polyfill for Call method
+ 
